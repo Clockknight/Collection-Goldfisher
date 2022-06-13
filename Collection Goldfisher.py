86f74b84322bb90292cbe14f961df9dd517ec365
Clockknight <tylerf2wang@gmail.com>
2022-06-12 18:58:13 -0700

    Skeleton of python script in comments

---
+++ Collection Goldfisher.py
@@ -0,0 +1,3 @@
+# Code takes in spreadsheet, creates 2d array full of cards and the amount of those cards available.
+# Scans Goldfish, creates lists of what cards are needed.
+# return a list of Decks, as well as what cards are needed, and the price of the cards still needed
